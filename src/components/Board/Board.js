36442ebc4882214fff6e155d552b464420121f15
Dylan Sharon <reachdylan@gmail.com>
2021-04-12 11:33:43 -0400

    Add button and board components, succesful render to page

---
+++ src/components/Board/Board.js
@@ -0,0 +1,15 @@
+import Tile from "../Tile/Tile";
+
+const Board = (props) => {
+    return (
+        <div>
+            <Tile />
+            <Tile />
+            <Tile />
+            <Tile />
+            <Tile />
+        </div>
+    );
+}
+
+export default Board;
---
+++ src/components/Tile/Tile.js
@@ -0,0 +1,11 @@
+import Button from "@material-ui/core/Button";
+
+const Tile = (props) => {
+    return (
+        <div>
+            <Button variant="outlined" color="primary">Tile</Button>
+        </div>
+    );
+}
+
+export default Tile;
